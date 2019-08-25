3128ceec660b7150547499cef5185f145d60d19e
Kyunghwa <adela914@gmail.com>
2019-08-25 21:39:45 +0200

    Finnished first version

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
+
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
-    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-        alert("I got clicked!");
+     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonInnerHtml = this.innerHTML;
+
+switch (buttonInnerHtml) {
+    case "w":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+    case "a":
+        var kickBass = new Audio("sounds/kick-bass.mp3");
+        kickBass.play();
+        break;
+    case "s":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+
+        break;
+    case "d":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+    case "j":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+    case "k":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+    case "l":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;                
+
+
+
+
+    default:
+        console.log(buttonInnerHtml);
+} 
+
     });
 
 }
@@ -20,4 +60,4 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
 //most common way is instead of adding a function name here that calls the function later on.
 //most people wirte this code as simply an anonymous function.
- 
+// QuerySelectorAll뒤에다가 button태그를 넣는 경우에는 나중에 추가적으로 만들어지는 이와 상관없는 버튼들까지 모두 소환되므로 피하는게 좋음!
